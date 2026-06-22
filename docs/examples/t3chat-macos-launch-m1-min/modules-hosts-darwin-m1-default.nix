b4221d8565823ee24df41c9b1c2e9901b20e50ea
drunkod <drunkod@gmail.com>
2026-06-22 16:15:42 +0500

    docs: add full m1 host example for t3chat launch

---
+++ docs/examples/t3chat-macos-launch-m1-min/modules-hosts-darwin-m1-default.nix
@@ -0,0 +1,81 @@
+{
+  config,
+  inputs,
+  ...
+}:
+
+let
+  host = {
+    name = "MacBookAirM1";
+    user.name = "test";
+    state = {
+      darwin = 4;
+      version = "22.05";
+    };
+    system = "aarch64-darwin";
+  };
+
+  minimalHost = host // {
+    name = "MacBookAirM1Minimal";
+  };
+
+  aiCoreImports = with config.flake.modules.homeManager; [
+    inputs.sops-nix.homeManagerModules.sops
+    sops
+    config.flake.modules.homeManager."claude-code"
+    config.flake.modules.homeManager.zed
+    mcp
+    zsh
+    graphify
+  ];
+
+  aiFullImports = aiCoreImports ++ [
+    config.flake.modules.homeManager.codex
+    config.flake.modules.homeManager."pi-coding-agent"
+    config.flake.modules.homeManager.proxypilot-t3chat
+  ];
+in
+{
+  flake.darwinConfigurations.m1 = inputs.darwin.lib.darwinSystem {
+    system = host.system;
+    specialArgs = { inherit inputs; };
+    modules = with config.flake.modules.darwin; [
+      base
+      m1
+
+      homebrewM1
+      aerospace
+      kitty
+      nixvim
+    ];
+  };
+
+  flake.modules.darwin.m1 = {
+    inherit host;
+    home-manager.users.${host.user.name} = {
+      imports = aiCoreImports;
+      services.sops.enable = true;
+    };
+  };
+
+  flake.darwinConfigurations.m1-min = inputs.darwin.lib.darwinSystem {
+    system = minimalHost.system;
+    specialArgs = { inherit inputs; };
+    modules = with config.flake.modules.darwin; [
+      base
+      m1-min
+
+      aerospace
+      homebrewM1Minimal
+      kitty
+    ];
+  };
+
+  flake.modules.darwin.m1-min = {
+    host = minimalHost;
+    home-manager.users.${minimalHost.user.name} = {
+      imports = aiFullImports;
+      services.sops.enable = true;
+    };
+  };
+}
