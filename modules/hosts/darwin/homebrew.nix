ddd2187c06f377b127633d3b2d599fa012465f18
drunkod <drunkod@gmail.com>
2026-08-11 13:20:08 +0500

    chore(pr9): drop unrelated Homebrew auto-update change

--- modules/hosts/darwin/homebrew.nix
+++ modules/hosts/darwin/homebrew.nix
@@ -12,7 +12,7 @@
         enableBashIntegration = true;
         enableZshIntegration = true;
         onActivation = {
-          autoUpdate = true;
+          autoUpdate = false;
           upgrade = false;
           # TODO: switch back to `cleanup = "zap"` once nix-darwin emits
           # `--cleanup --zap` instead of the removed `--force-cleanup` flag.
